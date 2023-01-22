c1532939c7a9718f029e7694c8506f8b225122a9
Gilvan <87739497+GilvanPedro@users.noreply.github.com>
2023-01-22 18:25:24 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+console.log(document);
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.scr;
+
+function alternarModal(){
+	modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+	alternarModal();
+	video.setAttribute("src", "");
+});
